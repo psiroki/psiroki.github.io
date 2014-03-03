be28a85b36d32ccd4a035f4bac839830f43cfbd7
psiroki <siroki@pegasus>
2014-03-03 23:10:15 +0100

    Added operator new

---
+++ ccjegyzet/oo/new.cc
@@ -0,0 +1,24 @@
+#include "point.h"
+char* repeat(char c, int count)
+{
+	// figyeljuk meg, hogy a tomb meretenek nem csak konstanst, hanem
+	// akarmilyen erteket megadhatunk
+	char *buf = new char[count+1];
+	memset(buf, c, count);
+	buf[count] = 0;
+	return buf;
+}
+int main()
+{
+	char *sleep = repeat('z', 10);
+	printf("%s\n", sleep);
+	// ne felejtsuk el felszabaditani!
+	// ha tombot allokaltunk, akkor a delete[] operatort hasznaljuk
+	delete[] sleep;
+	
+	Point *p = new Point(2, 2);
+	// ha nem tomb, akkor siman delete
+	delete p;
+	
+	return 0;
+}
