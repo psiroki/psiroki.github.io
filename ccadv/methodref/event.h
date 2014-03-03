4cf8de970b321070796cf91cb71b030063a1acf4
psiroki <siroki@pegasus>
2014-03-03 22:26:28 +0100

    Fxied for any member method pointer size (assuming a reasonable compiler)

--- ccadv/methodref/event.h
+++ ccadv/methodref/event.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <string.h>
+
 namespace event {
 
 template<class Event> class Slot
@@ -8,23 +10,36 @@ public:
 	typedef void (*EventCallback)(void *obj, const void *method, const Event &event);
 private:
 	void * obj;
-	const void *method;
+	// the key to this implementation is that it
+	// doesn't need any memory allocation, so we
+	// have to fit everything statically
+	// member function pointers may get as big as
+	// 20 bytes (depending on the compiler) with
+	// 32 bit addresses, so let's use allocate 32
+	// bytes, just to be safe (or double of that
+	// with 64 bit addresses)
+	char methodPointer[8*sizeof(void*)];
 	EventCallback callback;
 public:
 	/// Null slot
-	inline Slot(): obj(0), method(0), callback(0)
+	inline Slot(): obj(0), callback(0)
 	{
+		memset(methodPointer, 0, sizeof(methodPointer));
 	}
 	
-	inline Slot(void *obj, const void *method, EventCallback callback):
-		obj(obj), method(method), callback(callback)
+	inline Slot(void *obj, const void * const *methodPointer, int pointerSize, EventCallback callback):
+		obj(obj), callback(callback)
 	{
+		memcpy(this->methodPointer, methodPointer, pointerSize);
 	}
 
 	inline void operator()(const Event &event) const
 	{
-		if(method)
-			callback(obj, method, event);
+		if(callback)
+		{
+			const void *pp = methodPointer;
+			callback(obj, methodPointer, event);
+		}
 	}
 };
 
@@ -43,14 +58,15 @@ public:
 	
 	inline operator Slot<Event>()
 	{
-		return Slot<Event>(receiver, *reinterpret_cast<const void* const *>(&callback),
-			&handleCallbacks);
+		return Slot<Event>(receiver, reinterpret_cast<const void* const *>(&callback),
+			sizeof(callback), &handleCallbacks);
 	}
 	
 	inline static void handleCallbacks(void *obj, const void *method, const Event &event)
 	{
 		T *receiver(reinterpret_cast<T*>(obj));
-		Callback callback = *reinterpret_cast<const Callback*>(&method);
+		Callback callback;
+		memcpy(&callback, method, sizeof(callback));
 		(receiver->*callback)(event);
 	}
 };
