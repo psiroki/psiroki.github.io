7948f56ae097ae60ac25ca6b87dabfe5ce3d699f
psiroki <siroki@pegasus>
2014-03-23 08:25:21 +0100

    Added ostream formatting

--- ccjegyzet/io/format.cc
+++ ccjegyzet/io/format.cc
@@ -18,11 +18,46 @@ int main()
 	cout << "5. |" << setprecision(3) << pi << "| |" << pi << "|" << endl;
 	cout << "6. |" << setw(8) << setprecision(3) << pi*10.0f << "| |" << pi << "|" << endl;
 	cout << "." << endl;
+	// lehet a setw es a setprecision manipulatort hasznalni, ezek ekvivalensek
+	// az alabbi hivasokkal:
 	cout.precision(5);
 	cout.width(12);
-	cout << "." << endl;
-	// a width mindenre vonatkozik, a "7. |" stringre is
+	// a width mindenre vonatkozik, nem csak a szamokra
+	// de csak egy hivas erejeig
+	cout << "." << "|" << endl;
 	cout << "7. |" << pi << "|" << endl;
+	cout << "8. |" << setw(8) << showpos << pi << "|" << endl;
+	cout << "9. |" << setw(8) << noshowpos << pi << "|" << endl;
+	
+	cout << showbase;
+	cout << "10. " << hex << num << endl;
+	cout << "11. " << dec << num << endl;
+	cout << "12. " << oct << num << endl;
+	
+	// termeszetesen ugyanilyen neven van manipulator is
+	cout << noshowbase;
+	
+	cout << "13. " << 1.0e15 << endl;
+	cout << "14. " << hex << 0xcafe12 << endl;
+	
+	cout << uppercase;
+	
+	cout << "15. " << 1.0e15 << endl;
+	cout << "16. " << hex << 0xcafe12 << endl;
+	
+	cout << nouppercase;
+	
+	cout << "17. " << true << endl;
+	
+	cout << boolalpha;
+	
+	cout << "18. " << true << endl;
+	
+	cout << noboolalpha;
+	
+	cout << "19. |" << left << setw(8) << pi << "|" << endl;
+	cout << "20. |" << setw(8) << pi << "|" << endl;
+	cout << "21. |" << internal << showpos << setw(8) << pi << "|" << endl;
 	
 	return 0;
 }
